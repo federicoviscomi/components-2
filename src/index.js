545d8e933930e3c6688a36c9a1f9dbf310e590c9
Federico Viscomi <federico.viscomi@dh.com>
2019-02-04 11:01:36 +0000

    section 3 end

--- src/index.js
+++ src/index.js
@@ -5,17 +5,24 @@ import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
 
 const App = () => {
-  return (
-      <div className="ui container comments">
-        <ApprovalCard/>
-        <CommentDetail
-            author="Alex"
-            timeAgo="Today at 4:45PM"
-            content="Nice blog post"
-            avatar={faker.image.avatar()}
-        />
-      </div>
-  )
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <div>
+                    <h4>Warning</h4>
+                    Are you sure?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 4:45PM"
+                    content="Nice blog post"
+                    avatar={faker.image.avatar()}>
+                </CommentDetail>
+            </ApprovalCard>
+        </div>
+    )
 };
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
